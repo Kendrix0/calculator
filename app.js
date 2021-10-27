b68780782c10f049e494d3a04dcfa26d489e4d07
Kendrix0 <kevin.nguyen9917@gmail.com>
2021-10-27 11:09:05 -0500

    Cleaned some logic and improved flow of operations

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let x = '';
 let pastNum = '0';
 let mode = 'none';
 let answer = x;
+let lastPressed = ''
 
 function operate(num1, num2) {
     if (mode == 'power') {
@@ -25,9 +26,10 @@ function operate(num1, num2) {
     } else if (mode == '-') {
         answer = (parseFloat(num1) - parseFloat(num2));
     } else {
-        answer = parseFloat(num2);
+        answer = parseFloat(num2 || num1);
     }
     pastNum = answer;
+    current.innerHTML = pastNum
     return answer
 }
 
@@ -36,12 +38,37 @@ function cleanse() {
     current.innerHTML = '0';
 }
 
+function updateHistory(e) {
+    if (pastNum > 1000000) {
+        displayPast = pastNum.toExponential(5)
+    } else {
+        displayPast = pastNum
+    }
+    if (mode=='none') {
+        if (x === '') {
+            history.innerHTML = displayPast
+        } else {
+            history.innerHTML = parseFloat(x)
+        }
+    } else {
+        if (lastPressed = 'evaluate') {
+            history.innerHTML = displayPast + mode + x + '='
+        } else {
+            if (mode == 'power') {
+                history.innerHTML = displayPast + '^';
+            } else {
+                history.innerHTML = displayPast + e.innerHTML;
+            }
+        }
+    }
+}
+
 allClear.onclick = () => {
     history.innerHTML = ''
     cleanse();
     pastNum = '0'
-    x = ''
     mode = 'none'
+    answer = x;
 }
 
 clear.onclick = () => {
@@ -69,34 +96,66 @@ window.addEventListener('keydown', (e) => {
 operands.forEach(btn => {
     btn.onclick = () => {
         x += btn.innerHTML
-        current.innerHTML = x;
+        if (x >= 1000000000) {
+            displayX = parseFloat(x).toExponential(5).toString()
+            current.innerHTML = displayX;
+        } else {
+           current.innerHTML = x; 
+        }
     }
+    lastPressed = 'operand'
 })
 
 operators.forEach(btn => {
     btn.onclick = () => {
+        if (lastPressed != 'evaluate') {
+            operate(pastNum, x)
+        }
         x = '';
         mode = btn.innerHTML;
-        history.innerHTML = pastNum + btn.innerHTML;
+        equalsPressed = 'operator';
+        updateHistory(btn);
     }
 })
 
 sign.onclick = () => {
+    if (!equalsPressed) {
+        
+    }
     negX = parseFloat(answer) * -1;
     x = negX;
     current.innerHTML = x;
 }
 
 power.onclick = () => {
-    current.innerHTML = x + '^';
+    if (lastPressed != 'evaluate') {
+        operate(pastNum, x)
+    }
+    x = '';
+    mode = 'power';
+    equalsPressed = false;
+    updateHistory(power);
 }
 
 evaluate.onclick = () => {;
-    if (mode=='none') {
-        history.innerHTML = x
+    lastPressed = 'evaluate';
+    updateHistory(evaluate);
+    operate(pastNum, x);
+    if (answer >= 1000000000) {
+        displayX = parseFloat(answer).toExponential(5).toString();
+        current.innerHTML = displayX;
     } else {
-    history.innerHTML = pastNum + mode + x + '='
+       current.innerHTML = answer; 
     }
-    console.log(operate(pastNum, x));
-    current.innerHTML = answer
+    
 }
+
+// Need to correct how the sign change button behaves
+// Need to correct how operand entry behaves after evaluation (i.e evaluating 2+2
+//      then going straight into 4+5 should not display 24 then 5)
+
+// Need to correct pastNum behavior after pressing AC then operator(i.e AC -> 5 -> +
+//      should not display 0+... in history)
+
+// Need to correct updateHistory function so that it does not display
+//      pastNum operand x when x = '' (i.e 5+= since x is blank).
